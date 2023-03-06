21009de8d82633d33d443fead59dff8425a3fe93
steven <stevensvn25@student.uns.ac.id>
2023-03-06 10:04:33 +0700

    Pertemuan pertama

---
+++ pepen.py
@@ -0,0 +1,18 @@
+#variabel
+int = 19            ## angka bulat
+float = 1.2         ## desimal
+bool = 0            ## true false (biner)
+str = "siapa hayo 25"  ## karakter
+print(int)
+
+nama_dpn = "steven"
+nama_blkg = "alexander"
+data = {"nama_dpn": "steven", "nama_blkg": "pepen"}
+
+print("Nama lengkap saya adalah", nama_dpn, nama_blkg)
+
+print(nama_dpn)
+
+print(nama_blkg)
+
+print(f"nama saya adalah", nama_dpn, nama_blkg, "atau", {})
