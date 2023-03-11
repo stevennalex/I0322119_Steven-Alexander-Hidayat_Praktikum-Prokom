2adc39e82fb3adf4ae5c3be9ecb3e00e57c510be
steven <stevensvn25@student.uns.ac.id>
2023-03-11 13:57:06 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,13 @@
+# Tugas 1 Praktikum Prokom
+
+nama = "Steven Alexander Hidayat"
+nim = "I0322119"
+tempat_tinggal = "Pontianak"
+visualisasi = "Pontianak, kota yang dijuluki sebagai Kota Khatulistiwa. Jika digambarkan dengan singkat, Pontianak merupakan kota yang nyaman, asri, cukup panas, dan yang pasti ngangenin"
+harapan = "Harapan saya sebagai lulusan Teknik Industri adalah menjadi pribadi yang berguna bagi masyarakat sesuai dengan bidang yang saya kuasai. Selain itu saya juga berharap dengan bekal yang saya punya, saya dapat bekerja di bidang yang saya tekuni dengan bayaran yang bagus"
+
+print("Nama aku", nama)
+print("NIM aku", nim)
+print("Aku berasal dari", tempat_tinggal)
+print("Jika", tempat_tinggal, "digambarkan maka seperti...", visualisasi)
+print(harapan)
