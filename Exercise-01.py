048a6605eebc78d55c5313d842ad5244be94a5aa
steven <stevensvn25@student.uns.ac.id>
2023-03-11 14:01:39 +0700

    Exercise-01

--- Exercise-01.py
+++ Exercise-01.py
@@ -3,11 +3,11 @@
 nama = "Steven Alexander Hidayat"
 nim = "I0322119"
 tempat_tinggal = "Pontianak"
-visualisasi = "Pontianak, kota yang dijuluki sebagai Kota Khatulistiwa. Jika digambarkan dengan singkat, Pontianak merupakan kota yang nyaman, asri, cukup panas, dan yang pasti ngangenin"
+visualisasi = "Rumahku yang berada di Pontianak jika divisualisasikan akan seperti, rumah sederhana yang diisi dengan ayah dan anak-anaknya dengan seekor anjing bernama Cony. Rumah yang tidak mewah, tetapi membawa kenyamanan yang tiada tara"
 harapan = "Harapan saya sebagai lulusan Teknik Industri adalah menjadi pribadi yang berguna bagi masyarakat sesuai dengan bidang yang saya kuasai. Selain itu saya juga berharap dengan bekal yang saya punya, saya dapat bekerja di bidang yang saya tekuni dengan bayaran yang bagus"
 
 print("Nama aku", nama)
 print("NIM aku", nim)
 print("Aku berasal dari", tempat_tinggal)
-print("Jika", tempat_tinggal, "digambarkan maka seperti...", visualisasi)
+print(visualisasi)
 print(harapan)
